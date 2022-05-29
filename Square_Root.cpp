8b2a221bfadc6f2e13e8985f5e644c2f7d0bd5df
Kiarash Siahbandi <kiarashsiahbandi@gmail.com>
2022-05-29 15:36:28 +0430

    Add files via upload
    This program finds square root of a number by sqrt (math.h).
    Simple and useful.

---
+++ Square_Root.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main()
+{
+	float num, root;
+	cout << "Enter a number:	";
+	cin >> num;
+	root = sqrt(num);
+	cout << "Square root of " << num << " is " << root;
+	return 0;
+}
